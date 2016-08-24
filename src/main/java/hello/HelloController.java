f29c2cfa099c04ef20178e44c778e7fee6346253
Juan Manuel <juan.manuel.mp8@gmail.com>
2016-08-24 17:06:26 -0500

    Commit inicial

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public Persona index() {
+        return new Persona();
+    }
+    
+}
+class Persona{
+	private String name="Juan Manuel";
+	
+	public String getName(){
+		return name;
+	}
+}
